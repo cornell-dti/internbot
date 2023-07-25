bcb732e495991869a418b4283d01423d225ebf83
Daniel Wei <weidaniel15@gmail.com>
2023-07-24 20:38:12 -0500

    Add more cron

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,6 @@ export const dynamic = "force-dynamic";
 export default function Home() {
     return (
         <main className='relative flex min-h-screen flex-col items-center justify-center'>
-            <Link
-                href='#'
-                className='group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all'
-            >
-                <p>Sign In to Continue</p>
-            </Link>
             <h1 className='pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl'>
                 Internbot
             </h1>
@@ -23,6 +17,12 @@ export default function Home() {
                 A serverless Slackbot built on Prisma, Next.js, and PostgreSQL,
                 for Cornell DTI.
             </p>
+            <Link
+                href='#'
+                className='group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all'
+            >
+                <p>Sign In to Continue</p>
+            </Link>
         </main>
     );
 }
