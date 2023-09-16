f733c3eba0a230ffa37ada9dab735c54d29c52e5
Daniel Wei <dlw266@cornell.edu>
2023-09-16 18:54:52 -0400

    lets try again

---
+++ lib/data/admins.ts
@@ -0,0 +1,33 @@
+type admin = {
+    name?: string;
+    email: string;
+};
+
+const admins: admin[] = [
+    {
+        name: "Daniel Wei",
+        email: "dlw266@cornell.edu",
+    },
+    {
+        name: "Justin Kong",
+        email: "jk2338@cornell.edu",
+    },
+    {
+        name: "Julie Jeong",
+        email: "sj598@cornell.edu",
+    },
+    {
+        name: "Rohan Maheshwari",
+        email: "rm697@cornell.edu",
+    },
+    {
+        name: "Sarah Young",
+        email: "sy398@cornell.edu",
+    },
+    {
+        name: "Michelle Li",
+        email: "myl39@cornell.edu",
+    },
+];
+
+export default admins;
