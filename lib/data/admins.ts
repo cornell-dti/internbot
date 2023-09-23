aa4130eb28d48b45e427676ac85b91d930c40ffc
Daniel Wei <dlw266@cornell.edu>
2023-09-22 23:39:26 -0400

    change admin controls

--- lib/data/admins.ts
+++ lib/data/admins.ts
@@ -12,22 +12,6 @@ const admins: admin[] = [
         name: "Justin Kong",
         email: "jk2338@cornell.edu",
     },
-    {
-        name: "Julie Jeong",
-        email: "sj598@cornell.edu",
-    },
-    {
-        name: "Rohan Maheshwari",
-        email: "rm697@cornell.edu",
-    },
-    {
-        name: "Sarah Young",
-        email: "sy398@cornell.edu",
-    },
-    {
-        name: "Michelle Li",
-        email: "myl39@cornell.edu",
-    },
 ];
 
 export default admins;
